9597b073e66e31221c44515b56ef6f87a9dcde62
attiasaeed <AYounis@dataserve.com.sa>
2023-06-21 13:50:44 +0300

    Add Jace File

--- src/main/java/com/dataserve/archivemanagement/controller/ClassificationController.java
+++ src/main/java/com/dataserve/archivemanagement/controller/ClassificationController.java
@@ -28,31 +28,31 @@ public class ClassificationController {
         return ResponseEntity.ok().body(classifications);
     }
 
-    @PostMapping("/classifications")
-    public ResponseEntity<Classifications> createClassifications(@Valid @RequestBody Classifications classifications) {
-        return ResponseEntity.ok().body(classificationsService.save(classifications));
-    }
+//    @PostMapping("/classifications")
+//    public ResponseEntity<Classifications> createClassifications(@Valid @RequestBody Classifications classifications) {
+//        return ResponseEntity.ok().body(classificationsService.save(classifications));
+//    }
 
 
-    @PutMapping("/classifications/{id}")
-    public ResponseEntity<Classifications> updateClassifications(@PathVariable(value = "id") Long classificationsId,
-                                                                 @Valid @RequestBody Classifications classificationsDetails) throws ResourceNotFoundException {
-        Classifications classifications = classificationsService.findById(classificationsId)
-                .orElseThrow(() -> new ResourceNotFoundException("classifications not found for this id :: " + classificationsId));
-        classifications.setClassCode(classificationsDetails.getClassCode());
-        classifications.setClassArName(classificationsDetails.getClassArName());
-        classifications.setClassEnName(classificationsDetails.getClassEnName());
-        classifications.setParentID(classificationsDetails.getParentID());
-        final Classifications updatedClassifications = classificationsService.save(classifications);
-        return ResponseEntity.ok(updatedClassifications);
-    }
+//    @PutMapping("/classifications/{id}")
+//    public ResponseEntity<Classifications> updateClassifications(@PathVariable(value = "id") Long classificationsId,
+//                                                                 @Valid @RequestBody Classifications classificationsDetails) throws ResourceNotFoundException {
+//        Classifications classifications = classificationsService.findById(classificationsId)
+//                .orElseThrow(() -> new ResourceNotFoundException("classifications not found for this id :: " + classificationsId));
+//        classifications.setClassCode(classificationsDetails.getClassCode());
+//        classifications.setClassArName(classificationsDetails.getClassArName());
+//        classifications.setClassEnName(classificationsDetails.getClassEnName());
+//        classifications.setParentID(classificationsDetails.getParentID());
+//        final Classifications updatedClassifications = classificationsService.save(classifications);
+//        return ResponseEntity.ok(updatedClassifications);
+//    }
 
-    @DeleteMapping("/classifications/{id}")
-    public ResponseEntity deleteClassifications(@PathVariable(value = "id") Long classificationsId)
-            throws ResourceNotFoundException {
-        Classifications classifications = classificationsService.findById(classificationsId)
-                .orElseThrow(() -> new ResourceNotFoundException("classifications not found for this id :: " + classificationsId));
-        classificationsService.deleteById(classifications);
-        return ResponseEntity.noContent().build();
-    }
+//    @DeleteMapping("/classifications/{id}")
+//    public ResponseEntity deleteClassifications(@PathVariable(value = "id") Long classificationsId)
+//            throws ResourceNotFoundException {
+//        Classifications classifications = classificationsService.findById(classificationsId)
+//                .orElseThrow(() -> new ResourceNotFoundException("classifications not found for this id :: " + classificationsId));
+//        classificationsService.deleteById(classifications);
+//        return ResponseEntity.noContent().build();
+//    }
 }
