6bc18d199e7954ecb5d64616ca6e4b639b1bb5c6
MostafaBelal <143500423+mostafa-bela@users.noreply.github.com>
2025-01-14 00:20:03 +0300

    update Global Exception Handler

--- src/main/java/com/dataserve/archivemanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dataserve/archivemanagement/exception/GlobalExceptionHandler.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.dataserve.archivemanagement.config.ConfigUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +20,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    ConfigUtil configUtil;
+
+
     @ExceptionHandler(CustomServiceException.class)
     public ResponseEntity<APIResponseResult<Object>> handleCustomServiceException(CustomServiceException ex) {
         APIResponseResult<Object> response = new APIResponseResult<>(
@@ -26,6 +33,49 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> badCredentialsException(BadCredentialsException ex, WebRequest request) {
+        APIResponseResult<Object> response = new APIResponseResult<>(
+                null,
+                HttpStatus.UNAUTHORIZED.value(),
+                configUtil.getLocalMessage("bad.Credential", null)
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+        ErrorResponse errorDetails = new ErrorResponse(new Date(), ex.getMessage(), request.getDescription(false), HttpStatus.OK.value());
+        APIResponseResult<Object> response = new APIResponseResult<>(
+                null,
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            if (stringBuilder.length() > 0 )
+                stringBuilder.append(", ");
+            errorResponse.addValidationError(error.getField(), error.getDefaultMessage());
+            stringBuilder.append(error.getDefaultMessage());
+        }
+
+        APIResponseResult<Object> response = new APIResponseResult<>(
+                null,
+                HttpStatus.BAD_REQUEST.value(),
+                stringBuilder.toString()
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -49,28 +99,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), ex.getMessage(), request.getDescription(false), HttpStatus.INTERNAL_SERVER_ERROR.value());
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            if (stringBuilder.length() > 0 )
-                stringBuilder.append(", ");
-            errorResponse.addValidationError(error.getField(), error.getDefaultMessage());
-            stringBuilder.append(error.getDefaultMessage());
-        }
-        errorResponse.setTimestamp(new Date());
-        errorResponse.setMessage(stringBuilder.toString());
-        errorResponse.setDetails(request.getDescription(false));
-        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
 
 
 }
