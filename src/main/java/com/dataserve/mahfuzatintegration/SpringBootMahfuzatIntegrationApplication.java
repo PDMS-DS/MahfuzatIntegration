c3c521570076c52a9cefbabbf458b762b9f6f194
MostafaBelal <143500423+mostafa-bela@users.noreply.github.com>
2025-05-03 23:49:07 +0300

    generate war

--- src/main/java/com/dataserve/mahfuzatintegration/SpringBootMahfuzatIntegrationApplication.java
+++ src/main/java/com/dataserve/mahfuzatintegration/SpringBootMahfuzatIntegrationApplication.java
@@ -2,33 +2,35 @@ package com.dataserve.mahfuzatintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 // {{JAR-SETTING}}
+//@SpringBootApplication
+//@EnableAspectJAutoProxy(exposeProxy = true)
+//public class SpringBootMahfuzatIntegrationApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootMahfuzatIntegrationApplication.class, args);
+//	}
+//
+//}
+
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
-public class SpringBootMahfuzatIntegrationApplication {
+public class SpringBootMahfuzatIntegrationApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// Configure the application when deployed as a WAR
+		return application.sources(SpringBootMahfuzatIntegrationApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMahfuzatIntegrationApplication.class, args);
 	}
 
 }
-
-//@SpringBootApplication
-//@EnableAspectJAutoProxy(exposeProxy = true)
-//public class SpringBootMahfuzatIntegrationApplication extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		// Configure the application when deployed as a WAR
-//		return application.sources(SpringBootForArchiveSchApplication.class);
-//	}
-//
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootMahfuzatIntegrationApplication.class, args);
-//	}
-
-//}
